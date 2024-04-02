0d751b85361e16b3315209e3f8514bb2134d7ccd
cga-bgp <cga.barokahperkasagroup@gmail.com>
2024-04-02 08:33:50 +0800

    fix: fix create server

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import cors from "cors";
-import createServer from "http";
+import { createServer } from "http";
 
 import { userRouter } from "./user/user.router";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const server = createServer(app);
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/user", userRouter);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}!`)
 });
